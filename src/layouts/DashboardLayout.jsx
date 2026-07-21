14e6211200d6ed43978dcb889845e34ed1fe5c2e
Ashok maddula <162942607+798939ashok@users.noreply.github.com>
2026-07-21 13:16:17 +0530

    Dashboard changed

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
 import Topbar from '../components/Topbar'
@@ -17,16 +18,18 @@ const pageTitles = {
 export default function DashboardLayout() {
   const { pathname } = useLocation()
   const title = pageTitles[pathname] || 'InsightAI'
+  const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
     <div className="flex min-h-screen bg-canvas">
-      <Sidebar />
+      <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <div className="flex-1 flex flex-col min-w-0">
-        <Topbar title={title} />
-        <main className="flex-1 p-6">
+        <Topbar title={title} onMenuClick={() => setSidebarOpen(true)} />
+        <main className="flex-1 p-4 md:p-6">
           <Outlet />
         </main>
       </div>
     </div>
   )
-}
+        }
+            
